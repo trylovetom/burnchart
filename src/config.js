9f890b301c0da6b5ac2885b22b18eb015576b8cc
CHANG, TZU-YEN <try_love_tom@icloud.com>
2017-08-05 22:21:25 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -27,9 +27,9 @@ export default {
     // Days we are not working. Mon = 1
     "off_days": [ ],
     // How does a size label look like?
-    "size_label": /^size (\d+)$/,
+    "size_label": /^point: (\d+)$/,
     // Process all issues as one size (ONE_SIZE) or use labels (LABELS).
-    "points": 'ONE_SIZE'
+    "points": 'LABELS'
   },
   // Request pertaining.
   "request": {
